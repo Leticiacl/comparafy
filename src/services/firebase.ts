f173110775065c3b519ecb253878db1ac639b8b1
Leticiacl <leticia.lacerda@aluno.ufop.edu.br>
2025-06-22 18:55:27 -0300

    integração google

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+// src/services/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1rCUhbYR7UApioX8UvqBiyiLr_1UQKCI",
+  authDomain: "comparafy.firebaseapp.com",
+  projectId: "comparafy",
+  storageBucket: "comparafy.appspot.com",
+  messagingSenderId: "605554593459",
+  appId: "1:605554593459:web:927294878e9317ecb2eac6",
+  measurementId: "G-VE8E5YZXN0"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Autenticação + Google Provider
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
