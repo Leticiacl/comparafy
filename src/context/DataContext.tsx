96c8954a0c4ab55edb74c4a9835b95fd44e819ba
Leticiacl <leticia.lacerda@aluno.ufop.edu.br>
2025-07-20 22:45:44 -0300

    att

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -4,7 +4,7 @@ import {
   fetchLists,
   updateListName,
 } from '../services/firestoreService'
-import { auth } from '../../firebase' // <- caminho corrigido
+import { auth } from '../firebase' // ✅ Caminho corrigido aqui
 
 export interface ListType {
   id: string
